87c7d60237bdaf40ab92131775cd633942c106bb
PedroMakengo <pedromakengomiguel@gmail.com>
2022-12-24 10:32:14 -0300

    Trabalhando e aprendendo sobre a estrutura de pasta de um projeto VUE

--- 05-vue-cli/vue-cli-hello-world/src/App.vue
+++ 05-vue-cli/vue-cli-hello-world/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
@@ -9,14 +9,15 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial,
+    sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
