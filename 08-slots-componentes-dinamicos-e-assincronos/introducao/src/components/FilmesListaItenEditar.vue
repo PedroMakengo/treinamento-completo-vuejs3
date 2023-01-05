86d16516a7f88fe1e274967cfd0d4f10069c2366
PedroMakengo <pedromakengomiguel@gmail.com>
2023-01-05 14:42:37 -0300

    Aprendendo bem sobre os components

---
+++ 08-slots-componentes-dinamicos-e-assincronos/introducao/src/components/FilmesListaItenEditar.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <h2>Editar filme</h2>
+
+    <div class="form-group">
+      <label>Título:</label>
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Insira o título"
+        :value="filmeSelecionado.titulo"
+        @input="
+          filmeSelecionado = {
+            propriedade: 'titulo',
+            valor: $event.target.value,
+          }
+        "
+      />
+    </div>
+    <div class="form-group">
+      <label>Ano:</label>
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Insira o título"
+        :value="filmeSelecionado.ano"
+        @input="
+          filmeSelecionado = {
+            propriedade: 'ano',
+            valor: $event.target.value,
+          }
+        "
+      />
+    </div>
+
+    <button
+      @click="salvarFilme"
+      class="btn btn-primary float-right"
+    >
+      Salvar
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    filme: {
+      type: Object,
+      required: true,
+    },
+    filmeTitulo: String,
+  },
+  data() {
+    return {
+      filmeLocal: this.filme,
+    }
+  },
+  computed: {
+    filmeSelecionado: {
+      set(dados) {
+        this.filmeLocal = Object.assign(
+          {},
+          this.filmeLocal,
+          { [dados.propriedade]: dados.valor }
+        )
+      },
+      get() {
+        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+        return (this.filmeLocal = this.filme)
+      },
+    },
+  },
+
+  methods: {
+    salvarFilme() {
+      this.emitter.emit(
+        'atualizarFilme',
+        this.filmeLocal
+      )
+    },
+  },
+}
+</script>
