695816cc1788080f6b540e2c3c84be8f15b17216
PedroMakengo <pedromakengomiguel@gmail.com>
2023-01-13 16:38:42 -0300

    Aprendendo animação

--- 10-animando-elementos-e-components/vue-animacao/src/App.vue
+++ 10-animando-elementos-e-components/vue-animacao/src/App.vue
@@ -2,374 +2,58 @@
   <div id="app">
     <div class="jumbotron jumbotron-fluid">
       <div class="container">
-        <h1 class="display-4">
-          Formulários no Vue
-        </h1>
+        <h1 class="display-4">Animações</h1>
         <p class="lead">
-          Treinando a manipulação de formulários
+          Treinando transição/animação de
+          elementos/components no Vue.
         </p>
       </div>
     </div>
 
     <div class="container">
-      <div class="row">
-        <!-- formulario -->
-        <div class="col-sm-6">
-          <h3>Preencha abaixo</h3>
+      <button
+        class="btn btn-primary mb-3"
+        @click="mostrar = !mostrar"
+      >
+        Alternar
+      </button>
 
-          <form
-            @submit.prevent="enviar"
-            @reset="resetar"
-          >
-            <div class="form-group">
-              <label>Nome:</label>
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Seu nome"
-                v-model.lazy.trim="
-                  desenvolvedor.nome
-                "
-              />
-            </div>
-
-            <div class="form-group">
-              <label>Endereço de email:</label>
-              <input
-                type="email"
-                class="form-control"
-                placeholder="Seu email"
-                v-model.lazy="desenvolvedor.email"
-              />
-            </div>
-
-            <div class="form-group">
-              <label>Idade:</label>
-              <input
-                type="number"
-                class="form-control"
-                placeholder="Sua idade"
-                v-model.number="
-                  desenvolvedor.idade
-                "
-              />
-            </div>
-
-            <div class="form-group">
-              <p>Gênero:</p>
-
-              <div
-                class="form-check form-check-inline"
-              >
-                <input
-                  type="radio"
-                  class="form-check-input"
-                  value="Masculino"
-                  v-model="desenvolvedor.genero"
-                />
-                <label class="form-check-label"
-                  >Masculino</label
-                >
-              </div>
-
-              <div
-                class="form-check form-check-inline"
-              >
-                <input
-                  type="radio"
-                  class="form-check-input"
-                  value="Feminino"
-                  v-model="desenvolvedor.genero"
-                />
-                <label class="form-check-label"
-                  >Feminino</label
-                >
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label>Ocupação:</label>
-              <select
-                class="form-control"
-                v-model="desenvolvedor.ocupacao"
-              >
-                <option value="" disabled>
-                  Selecione uma opção...
-                </option>
-                <option
-                  v-for="(
-                    ocupacao, index
-                  ) in ocupacoes"
-                  :key="index"
-                  :value="ocupacao"
-                >
-                  {{ ocupacao }}
-                </option>
-              </select>
-            </div>
-
-            <div class="form-group">
-              <p>Tecnologias:</p>
-
-              <div
-                class="form-check form-check-inline"
-              >
-                <input
-                  type="checkbox"
-                  class="form-check-input"
-                  value="JavaScript"
-                  v-model="
-                    desenvolvedor.tecnologias
-                  "
-                />
-                <label class="form-check-label"
-                  >JavaScript</label
-                >
-              </div>
-
-              <div
-                class="form-check form-check-inline"
-              >
-                <input
-                  type="checkbox"
-                  class="form-check-input"
-                  value="Vue JS"
-                  v-model="
-                    desenvolvedor.tecnologias
-                  "
-                />
-                <label class="form-check-label"
-                  >Vue JS</label
-                >
-              </div>
-
-              <div
-                class="form-check form-check-inline"
-              >
-                <input
-                  type="checkbox"
-                  class="form-check-input"
-                  value="Vuex"
-                  v-model="
-                    desenvolvedor.tecnologias
-                  "
-                />
-                <label class="form-check-label"
-                  >Vuex</label
-                >
-              </div>
-
-              <div
-                class="form-check form-check-inline"
-              >
-                <input
-                  type="checkbox"
-                  class="form-check-input"
-                  value="Vue Router"
-                  v-model="
-                    desenvolvedor.tecnologias
-                  "
-                />
-                <label class="form-check-label"
-                  >Vue Router</label
-                >
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label>Resumo de perfil:</label>
-              <textarea
-                class="form-control"
-                placeholder="Conte-nos um pouco sobre você..."
-                v-model="desenvolvedor.biografia"
-              ></textarea>
-            </div>
-
-            <div class="form-group">
-              <AppRange
-                label="Salário pretendido"
-                v-model:value.number="
-                  desenvolvedor.salario
-                "
-                min="10000"
-                max="15000"
-                step="500"
-                inputClasses="form-control-range"
-              />
-            </div>
-
-            <div class="form-group">
-              <div
-                class="form-check form-check-inline"
-              >
-                <input
-                  type="checkbox"
-                  class="form-check-input"
-                  v-model="
-                    desenvolvedor.notificacoes
-                  "
-                  true-value="Sim"
-                  false-value="Não"
-                />
-                <label class="form-check-label"
-                  >Receber notificações por
-                  email</label
-                >
-              </div>
-            </div>
-
-            <button
-              class="btn btn-secondary"
-              type="reset"
-            >
-              Resetar
-            </button>
-            <button
-              class="btn btn-success"
-              type="submit"
-            >
-              Enviar
-            </button>
-
-            <!-- <button
-              class="btn btn-success"
-              type="button"
-              @click.prevent="enviar"
-            >
-              Enviar
-            </button> -->
-          </form>
+      <transition>
+        <div
+          class="alert alert-primary"
+          v-if="mostrar"
+        >
+          Animações no Vue
         </div>
-
-        <!-- saida -->
-        <div class="col-sm-6">
-          <h3>Saída</h3>
-
-          <div class="card">
-            <div class="card-header">Dados</div>
-
-            <ul
-              class="list-group list-group-flush"
-            >
-              <li class="list-group-item">
-                <strong>Nome:</strong>
-                {{ desenvolvedor.nome }}
-              </li>
-              <li class="list-group-item">
-                <strong>Email:</strong>
-                {{ desenvolvedor.email }}
-              </li>
-              <li class="list-group-item">
-                <strong>Idade:</strong>
-                {{ desenvolvedor.idade }}
-              </li>
-              <li class="list-group-item">
-                <strong>Gênero:</strong>
-                {{ desenvolvedor.genero }}
-              </li>
-              <li class="list-group-item">
-                <strong>Ocupação:</strong>
-                {{ desenvolvedor.ocupacao }}
-              </li>
-              <li class="list-group-item">
-                <strong>Tecnologias:</strong>
-                <ul>
-                  <li
-                    v-for="(
-                      tecnologia, index
-                    ) in desenvolvedor.tecnologias"
-                    :key="index"
-                  >
-                    {{ tecnologia }}
-                  </li>
-                </ul>
-              </li>
-              <li class="list-group-item">
-                <strong>Biografia:</strong>
-                <div style="white-space: pre">
-                  {{ desenvolvedor.biografia }}
-                </div>
-              </li>
-              <li class="list-group-item">
-                <strong
-                  >Receber notificações?</strong
-                >
-                {{ desenvolvedor.notificacoes }}
-              </li>
-              <li class="list-group-item">
-                <strong
-                  >Salário pretendido:</strong
-                >
-                {{ desenvolvedor.salario }}
-              </li>
-            </ul>
-
-            <div class="card-header">Model</div>
-
-            <div class="card-body">
-              <pre><code>{{ desenvolvedor }}</code></pre>
-            </div>
-          </div>
-        </div>
-      </div>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
-import AppRange from '@/components/Range.vue'
-
 export default {
   data() {
     return {
-      desenvolvedor: {},
-      valoresPadroes: {
-        nome: '',
-        email: '',
-        idade: 23,
-        biografia: 'Sou desenvolvedor desde 2016',
-        genero: 'Masculino',
-        tecnologias: [],
-        notificacoes: 'Não',
-        ocupacao: '',
-        salario: 1000,
-      },
-      ocupacoes: [
-        'Desenvolvedor Front End (Web)',
-        'Desenvolvedor Front End (Mobile)',
-        'Desenvolvedor Front End (Web e Mobile)',
-        'Desenvolvedor Back End',
-        'Desenvolvedor Full Stack',
-      ],
+      mostrar: true,
     }
-  },
-  components: { AppRange },
-  methods: {
-    enviar() {
-      const formularioEnviado = Object.assign(
-        {},
-        this.desenvolvedor
-      )
-      console.log(
-        'Formulário enviado',
-        formularioEnviado
-      )
-    },
-    resetar() {
-      this.desenvolvedor = Object.assign(
-        {},
-        this.valoresPadroes
-      )
-    },
-  },
-  created() {
-    this.resetar()
   },
 }
 </script>
 
 <style scoped>
-.btn {
-  margin-right: 5px;
+/* Entrada */
+.v-enter {
+}
+.v-enter-active {
+}
+.v-enter-to {
+}
+
+/* Saída */
+.v-leave {
+}
+.v-leave-active {
+}
+.v-leave-to {
 }
 </style>
